35c8e69a8be917ca6d17b6793020bade027dfbae
maksimkaJCHK <maksimkajchk@rambler.ru>
2025-09-04 16:02:08 +0300

    Сделал болванку для сайта

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    Vue code markup
+  </header>
+
+  <main>
+    <h1>Vue code markup</h1>
+    <h2>Vue code markup</h2>
+
+    <p>ffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofd</p>
+
+    <p>ffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofdffdkiol idfiiofdiofd</p>
+  </main>
+
+  <footer>
+    <a
+      href="https://github.com/maksimkaJCHK/vue-code-markup"
+      target="_blank"
+      title="Vue code markup repository"
+    >Vue code markup</a>
+  </footer>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+
+</style>
