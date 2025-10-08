41199dbfd91d40e5d4211c9a54258623781d20e3
maksimkaJCHK <maksimkajchk@rambler.ru>
2025-10-08 20:39:49 +0300

    Поправил пути для сборки проекта

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
+
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+
+import terser from '@rollup/plugin-terser';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,6 +18,25 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'js/main.js',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'css/styles[extname]';
+          }
+
+          return 'images/[name][extname]';
+        },
+        plugins: [terser({
+          format: {
+            comments: false
+          },
+        })]
+      },
     },
   },
 })
