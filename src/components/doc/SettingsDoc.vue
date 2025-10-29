784e5064e43c9dba36d2d36f4f65b3156a0e5739
maksimkaJCHK <maksimkajchk@rambler.ru>
2025-10-29 22:02:55 +0300

    Начал делать блок настроек

--- src/components/doc/SettingsDoc.vue
+++ src/components/doc/SettingsDoc.vue
@@ -1,10 +1,49 @@
 <template>
-  <div class="intro-doc">
+  <div class="settings-doc">
     <template v-if="props.isRus">
       <h2>
         Пример настройки
       </h2>
 
+      <div class="settings-doc-row">
+        <div class="settings-doc-col settings-doc-code">
+          <code-markup>
+            <books-list-body />
+          </code-markup>
+        </div>
+        <div class="settings-doc-col settings-doc-content">
+          <div class="settings-doc-row">
+            <strong>header</strong>
+          </div>
+          <div class="settings-doc-row">
+            <strong>isHeader</strong>
+          </div>
+          <div class="settings-doc-row">
+            <strong>isCount</strong>
+          </div>
+          <div class="settings-doc-row">
+            <strong>lineCount</strong>
+          </div>
+          <div class="settings-doc-row">
+            <strong>textBold</strong>
+          </div>
+          <div class="settings-doc-row">
+            <strong>headerBold</strong>
+          </div>
+          <div class="settings-doc-row">
+            <strong>code</strong>
+          </div>
+          <div class="settings-doc-row">
+            <strong>title</strong>
+          </div>
+          <div class="settings-doc-row">
+            <strong>successfulText</strong>
+          </div>
+          <div class="settings-doc-row">
+            <strong>errorText</strong>
+          </div>
+        </div>
+      </div>
     </template>
 
     <template v-if="!props.isRus">
@@ -16,7 +55,7 @@
 </template>
 
 <script setup>
-  import BooksList from '@/components/code-examples/books/BooksList.vue';
+  import BooksListBody from '@/components/code-examples/books/BooksListBody.vue';
 
   const props = defineProps({
     isRus: {
@@ -27,5 +66,31 @@
 </script>
 
 <style lang="scss" scoped>
+  .settings-doc {
+    &-row {
+      display: grid;
+      gap: 10px 20px;;
+      grid-template-columns: 1fr 1fr;
 
+      @media screen and (max-width: 700px) {
+        grid-template-columns: 1fr;
+      }
+    }
+    
+    &-col {
+      .code-markup {
+        margin: 0;
+      }
+    }
+
+    @media screen and (max-width: 700px) {
+      &-code {
+        grid-row-start: 2;
+      }
+
+      &-content {
+        grid-row-start: 1;
+      }
+    }
+  }
 </style>
