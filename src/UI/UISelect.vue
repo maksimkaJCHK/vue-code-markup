284a567908dadc4f912a936e17a10ab86213658d
maksimkaJCHK <maksimkajchk@rambler.ru>
2025-10-31 19:43:41 +0300

    Сделал компонент select, не много дописал документацию

---
+++ src/UI/UISelect.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="ui-select">
+    <select v-model="iValue">
+      <option
+        v-for="item in props.options"
+        :key="item.id"
+        :value="item.id"
+      >
+        {{ item.title }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script setup>
+  const iValue = defineModel();
+
+  const props = defineProps(['options']);
+</script>
+
+<style lang="scss">
+  .ui-select {
+    select {
+      width: 100%;
+      padding: 5px;
+      display: block;
+      border-radius: 5px;
+      outline: none;
+      border: 1px solid grey;
+
+      &:focus {
+        box-shadow: 0 0 10px 0 rgba(55, 90, 127, .7);
+      }
+    }
+  }
+</style>
--- src/components/doc/SettingsDoc.vue
+++ src/components/doc/SettingsDoc.vue
@@ -12,6 +12,11 @@
             :is-header="isHeader"
             :is-count="isCount"
             :line-count="lineCount"
+            :text-bold="textBold"
+            :header-bold="headerBold"
+            :title="title"
+            :successful-text="successfulText"
+            :error-text="errorText"
           >
             <books-list-body />
           </code-markup>
@@ -50,26 +55,61 @@
           </div>
           <div class="settings-doc-row">
             <p>
-              <strong>lineCount</strong> - данный параметр отвечает за то, сколько строк отображать. В него нужно передавать числа, если нужно отобразить все строки, то нужно задать значение "auto". По умолчанию хадано значение "auto".
+              <strong>lineCount</strong> - данный параметр отвечает за то, сколько строк отображать. В него нужно передавать числа, если нужно отобразить все строки, то нужно задать значение "auto". По умолчанию задано значение "auto".
             </p>
+
+            <ui-select
+              v-model="lineCount"
+              :options="lineCountOptions"
+            />
           </div>
           <div class="settings-doc-row">
-            <strong>textBold</strong>
+            <p>
+              <strong>textBold</strong> - данный параметр отвечает за "толщину шрифта". По умолчанию равно "false".
+            </p>
+
+            <ui-checkbox
+              v-model="textBold"
+              value="textBold"
+            >
+              {{ textBold }}
+            </ui-checkbox>
           </div>
           <div class="settings-doc-row">
-            <strong>headerBold</strong>
+            <p>
+              <strong>headerBold</strong> - данный параметр отвечает за толщину шрифта заголовка окна с кодом. По умолчанию равно "false".
+            </p>
+
+            <ui-checkbox
+              v-model="headerBold"
+              value="headerBold"
+            >
+              {{ headerBold }}
+            </ui-checkbox>
           </div>
           <div class="settings-doc-row">
-            <strong>code</strong>
+            <strong>code</strong> - в данный параметр передается код, который нужно будет скопировать в буфер обмена. Это может быть как строка, так и объект.
           </div>
           <div class="settings-doc-row">
-            <strong>title</strong>
+            <p>
+              <strong>title</strong> - подсказка, которая будет появляться при наведении на иконку "скопировать текст". По умолчанию задано значение "Copy code to clipboard". Как правило это нужно для интернационализации.
+            </p>
+
+            <ui-input v-model="title" />
           </div>
           <div class="settings-doc-row">
-            <strong>successfulText</strong>
+            <p>
+              <strong>successfulText</strong> - подсказка, которая будет появляться при наведении на иконку "скопировать текст" после того, как текст будет скопирован. По умолчанию задано значение "The code is copied to the clipboard". Как правило это нужно для интернационализации.
+            </p>
+
+            <ui-input v-model="successfulText" />
           </div>
           <div class="settings-doc-row">
-            <strong>errorText</strong>
+            <p>
+              <strong>errorText</strong> - подсказка, которая будет появляться при наведении на иконку "скопировать текст" в случае возникновении ошибки (если текст не получится скопировать). По умолчанию задано значение "An error occurred while copying the code to the clipboard". Как правило это нужно для интернационализации.
+            </p>
+
+            <ui-input v-model="errorText" />
           </div>
         </div>
       </div>
@@ -89,6 +129,7 @@
   import BooksListBody from '@/components/code-examples/books/BooksListBody.vue';
   import UiInput from '@/UI/UIInput.vue';
   import UiCheckbox from '@/UI/UICheckbox.vue';
+  import UiSelect from '@/UI/UISelect.vue';
 
   const props = defineProps({
     isRus: {
@@ -97,10 +138,38 @@
     }
   });
 
+  function* bLineCountOptions(from=5, stop=60, step=5) {
+    let count = from;
+
+    while (count <= stop) {
+      const bVal = count + step;
+
+      yield {
+        id: bVal,
+        title: bVal
+      };
+
+      count = bVal;
+    }
+  };
+
+  const lineCountOptions = [
+    {
+      id: 'auto',
+      title: 'auto'
+    },
+    ...bLineCountOptions()
+  ];
+
   const header = ref('BooksList.vue');
   const isHeader = ref(true);
   const isCount = ref(true);
-  const lineCount = ref(20);
+  const lineCount = ref(50);
+  const textBold = ref(false);
+  const headerBold = ref(false);
+  const title = ref('Copy code to clipboard');
+  const successfulText = ref('The code is copied to the clipboard');
+  const errorText = ref('An error occurred while copying the code to the clipboard');
 </script>
 
 <style lang="scss" scoped>
