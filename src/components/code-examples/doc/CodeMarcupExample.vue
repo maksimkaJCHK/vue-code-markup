1156e4f018ec7906067e81bce6704f0d91b9e849
maksimkaJCHK <maksimkajchk@rambler.ru>
2025-11-10 20:11:29 +0300

    Продолжил писать документацию

---
+++ src/components/code-examples/doc/CodeMarcupExample.vue
@@ -0,0 +1,81 @@
+<template>
+  <code-markup
+    header="ExampleComponent.vue"
+    :code="code"
+    v-bind="codeParam"
+  >
+    <code-line>
+      <mu-tag code="<template>" />
+    </code-line>
+
+    <code-line level-2>
+      <mu-tag code="<code-markup>" />
+    </code-line>
+    <code-line level-4>
+      <mu-comment code="<!-- component content -->" />
+    </code-line>
+    <code-line level-2>
+      <mu-tag code="</code-markup>" />
+    </code-line>
+
+    <code-line>
+      <mu-tag code="</template>" />
+    </code-line>
+
+    <code-line />
+
+    <code-line>
+      <mu-tag code="<script " />
+      <mu-attr code="setup" />
+      <mu-tag code=">" />
+    </code-line>
+
+    <code-line level-2>
+      <mu-key-words code="import" />
+      <mu-type code=" CodeMarkup " />
+      <mu-key-words code="from" />
+      <mu-text code=" 'vue-code-markup'" />
+      <mu-type code=";" />
+    </code-line>
+
+    <code-line level-2>
+      <mu-key-words code="import" />
+      <mu-text code=" 'vue-code-markup/lib/style.css'" />
+      <mu-type code=";" />
+    </code-line>
+    <code-line>
+      <mu-tag code="</script>" />
+    </code-line>
+  </code-markup>
+</template>
+
+<script setup>
+  import useLang from '../useLang';
+
+  const props = defineProps({
+    lang: {
+      type: String,
+      default: 'en',
+      validator(value) {
+        return ['ru', 'en'].includes(value);
+      }
+    }
+  });
+
+  const { codeParam } = useLang(props);
+
+  const code = `<template>
+  <code-markup>
+    <!-- component content -->
+  </code-markup>
+</template>
+
+<script setup>
+  import CodeMarkup from 'vue-code-markup';
+  import 'vue-code-markup/lib/style.css';
+</${'script'}>`;
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/doc/SettingsDoc.vue
+++ src/components/doc/SettingsDoc.vue
@@ -6,14 +6,30 @@
       </h2>
 
       <p>
-        Это самый главный компонент, он служит обёрткой для всех остальных компонентов. 
+        Это самый главный компонент, он служит обёрткой для всех остальных компонентов. Если вы не хотите делать полную установку, то подключить его можно следующим способом:
+      </p>
+
+      <code-marcup-example lang="ru" />
+
+      <p>
+        Данный компонент имеет следующие входные параметры:
       </p>
     </template>
 
     <template v-if="!props.isRus">
       <h2>
-        Configuration example
+        The code-markup component
       </h2>
+
+      <p>
+        This is the most important component, it serves as a wrapper for all other components. If you do not want to do a full installation, then you can connect it in the following way:
+      </p>
+
+      <code-marcup-example />
+
+      <p>
+        This component has the following input parameters:
+      </p>
     </template>
 
     <div class="settings-doc-cont">
@@ -39,6 +55,10 @@
             <strong>isHeader</strong> - данный параметр отвечает за отображение заголовка окна с кодом. Если вы собираетесь отображать заголовок, то данный параметр можно не задавать, он по умолчанию равен "true".
           </p>
 
+          <p v-if="!props.isRus">
+            <strong>isHeader</strong> - this parameter is responsible for displaying the title of the window with the code. If you are going to display the title, then you do not need to set this parameter, it is set to "true" by default.
+          </p>
+
           <ui-checkbox
             v-model="isHeader"
             value="isHeader"
@@ -51,11 +71,19 @@
             <strong>header</strong> - заголовок окна с кодом. По умолчанию не имеет значения. Для примера я задал значение "BooksList.vue".
           </p>
 
+          <p v-if="!props.isRus">
+            <strong>header</strong> is the title of the window with the code. It doesn't matter by default. For example, I set the value to "BooksList.vue".
+          </p>
+
           <ui-input v-model="header" />
         </div>
         <div class="settings-doc-row">
           <p v-if="props.isRus">
             <strong>isCount</strong> - данный параметр отвечает за отображение номера строки. Если его не задать, то строки будут пронумерованы, по умолчанию равен "true".
+          </p>
+
+          <p v-if="!props.isRus">
+            <strong>isCount</strong> - this parameter is responsible for displaying the row number. If it is not set, the rows will be numbered, the default value is "true".
           </p>
 
           <ui-checkbox
@@ -66,8 +94,12 @@
           </ui-checkbox>
         </div>
         <div class="settings-doc-row">
-          <p>
-            <strong>lineCount</strong> - данный параметр отвечает за то, сколько строк отображать. В него нужно передавать числа, если нужно отобразить все строки, то нужно задать значение "auto". По умолчанию задано значение "auto". Для примера я задал {{ lineCount }} строк.
+          <p v-if="props.isRus">
+            <strong>lineCount</strong> - данный параметр отвечает за то, сколько строк отображать. В него нужно передавать числа, если нужно отобразить все строки, то нужно задать значение "auto", или вообще не задавать данный параметр. По умолчанию задано значение "auto". Для примера я задал {{ lineCount }} строк, и сделал интервал вывода от 10 до 65 строк.
+          </p>
+
+          <p v-if="!props.isRus">
+            <strong>lineCount</strong> - this parameter is responsible for how many lines to display. You need to pass numbers to it, if you want to display all the lines, then you need to set the value to "auto", or not set this parameter at all. The default value is "auto". For example, I set {{lineCount }} lines, and made an output interval from 10 to 65 lines.
           </p>
 
           <ui-select
@@ -76,8 +108,12 @@
           />
         </div>
         <div class="settings-doc-row">
-          <p>
-            <strong>textBold</strong> - данный параметр отвечает за "толщину шрифта". По умолчанию равно "false".
+          <p v-if="props.isRus">
+            <strong>textBold</strong> - данный параметр отвечает за "толщину шрифта". По умолчанию задано "false".
+          </p>
+
+          <p v-if="!props.isRus">
+            <strong>textBold</strong> - this parameter is responsible for the "font thickness". The default value is set to "false".
           </p>
 
           <ui-checkbox
@@ -88,8 +124,12 @@
           </ui-checkbox>
         </div>
         <div class="settings-doc-row">
-          <p>
-            <strong>headerBold</strong> - данный параметр отвечает за толщину шрифта заголовка окна с кодом. По умолчанию равно "false".
+          <p v-if="props.isRus">
+            <strong>headerBold</strong> - данный параметр отвечает за толщину шрифта заголовка окна с кодом. По умолчанию задано "false".
+          </p>
+
+          <p v-if="!props.isRus">
+            <strong>headerBold</strong> - this parameter is responsible for the font thickness of the title of the window with the code. The default value is set to "false".
           </p>
 
           <ui-checkbox
@@ -99,26 +139,46 @@
             {{ headerBold }}
           </ui-checkbox>
         </div>
+
         <div class="settings-doc-row">
-          <strong>code</strong> - в данный параметр передается код, который нужно будет скопировать в буфер обмена. Это может быть как строка, так и объект.
+          <template v-if="props.isRus">
+            <strong>code</strong> - в данный параметр передаётся код, который нужно будет скопировать в буфер обмена. Это может быть как строка, так и объект.
+          </template>
+
+          <template v-if="!props.isRus">
+            <strong>code</strong> - a code is passed to this parameter, which will need to be copied to the clipboard. It can be either a string or an object.
+          </template>
         </div>
+
         <div class="settings-doc-row">
-          <p>
+          <p v-if="props.isRus">
             <strong>title</strong> - подсказка, которая будет появляться при наведении на иконку "скопировать текст". По умолчанию задано значение "Copy code to clipboard". Как правило это нужно для интернационализации.
+          </p>
+
+          <p v-if="!props.isRus">
+            <strong>title</strong> - a hint that will appear when you hover over the "copy text" icon. The default value is "Copy code to clipboard". As a rule, this is necessary for internationalization.
           </p>
 
           <ui-input v-model="title" />
         </div>
         <div class="settings-doc-row">
-          <p>
+          <p v-if="props.isRus">
             <strong>successfulText</strong> - подсказка, которая будет появляться при наведении на иконку "скопировать текст" после того, как текст будет скопирован. По умолчанию задано значение "The code is copied to the clipboard". Как правило это нужно для интернационализации.
+          </p>
+
+          <p v-if="!props.isRus">
+            <strong>successfulText</strong> is a hint that will appear when you hover over the "copy text" icon after the text has been copied. The default value is "The code is copied to the clipboard". As a rule, this is necessary for internationalization.
           </p>
 
           <ui-input v-model="successfulText" />
         </div>
         <div class="settings-doc-row">
-          <p>
+          <p v-if="props.isRus">
             <strong>errorText</strong> - подсказка, которая будет появляться при наведении на иконку "скопировать текст" в случае возникновении ошибки (если текст не получится скопировать). По умолчанию задано значение "An error occurred while copying the code to the clipboard". Как правило это нужно для интернационализации.
+          </p>
+
+          <p v-if="!props.isRus">
+            <strong>errorText</strong> - a hint that will appear when hovering over the "copy text" icon in case of an error (if the text cannot be copied). By default, the value is set to "An error occurred while copying the code to the clipboard". As a rule, this is necessary for internationalization.
           </p>
 
           <ui-input v-model="errorText" />
@@ -133,6 +193,7 @@
 
   import useLang from '@/components/code-examples/uselang.js';
 
+  import CodeMarcupExample from '@/components/code-examples/doc/CodeMarcupExample.vue';
   import BooksListBody from '@/components/code-examples/books/BooksListBody.vue';
   import { bookListCode } from '@/components/code-examples/books/BookListCode.js';
 
