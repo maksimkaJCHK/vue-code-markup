e31631900f89f942f2d294924f9c7436cb7db6cf
maksimkaJCHK <maksimkajchk@rambler.ru>
2025-11-21 20:50:51 +0300

    Описываю компонент code-line

--- src/components/doc/CodeLineDoc.vue
+++ src/components/doc/CodeLineDoc.vue
@@ -8,21 +8,41 @@
       </p>
 
       <code-line-example lang="ru" />
+
+      <p>
+        Данный компонент имеет следующие входные параметры:
+      </p>
     </template>
 
     <template v-if="!props.isRus">
       <h2>The code-line component</h2>
 
       <p>
-        По сути это линия или строка с кодом. Элементы кода должны находиться в этом компоненте. Подключается он следующим способом:
+        In fact, this is a line or a line with a code. The code elements must be located in this component. It is connected in the following way:
       </p>
 
       <code-line-example lang="en" />
+
+      <p>
+        This component has the following input parameters:
+      </p>
     </template>
+
+    <settings-doc>
+      <template #code>
+        <code-markup :is-header="false">
+          <code-line>
+            Example of the line-count component for displaying input parameters
+          </code-line>
+        </code-markup>
+      </template>
+      <template #content></template>
+    </settings-doc>
   </div>
 </template>
 
 <script setup>
+  import SettingsDoc from '@/components/SettingsDoc.vue';
   import CodeLineExample from '@/components/code-examples/doc/CodeLineExample.vue';
 
   const props = defineProps({
