6726f31e6b3c65042f72568e6774275fcaf84e16
maksimkaJCHK <maksimkajchk@rambler.ru>
2025-12-05 14:45:24 +0300

    Взял пример для блока стидей

--- src/components/doc/StyleDoc.vue
+++ src/components/doc/StyleDoc.vue
@@ -87,12 +87,80 @@
       :lineCount="15"
     >
       <template #output>
+        <code-line>
+          <mu-style-id code="#books-list {" />
+        </code-line>
+        <code-line level-2>
+          <mu-style-param code="margin" />:
+          <mu-number code=" 1" />
+          <mu-style-unit-meas code="em" />
+          <mu-number code=" 0 " />
+          <mu-number code="2.5" />
+          <mu-style-unit-meas code="em" />;
+        </code-line>
+        <code-line>
+          <mu-style-id code="}" />
+        </code-line>
+
+        <code-line />
+
+        <code-line>
+          <mu-style-class code=".books-list {" />
+        </code-line>
+
+        <code-line level-2>
+          <mu-style-param code="line-height" />:
+          <mu-number code=" 1.2" />;
+        </code-line>
+
+        <code-line />
+
+        <code-line level-2>
+          <mu-style-tag code="h3 {" />
+        </code-line>
+        <code-line level-2>
+          <mu-style-param code="font-size" />:
+          <mu-number code="1.2" />
+          <mu-style-unit-meas code="em" />;
+        </code-line>
+        <code-line level-3>
+          <mu-style-param code="margin" />:
+          <mu-number code=".4" />
+          <mu-style-unit-meas code="em" />
+          <mu-number code=" 0 " />
+          <mu-number code=".3" />
+          <mu-style-unit-meas code="em" />;
+        </code-line>
+        <code-line level-2 >
+          <mu-style-tag code="}" />
+        </code-line>
+
+        <code-line />
+
+        <code-line level-2>
+          <mu-style-ampersand code="&" />
+          <mu-style-key code="-item" />
+          <mu-style-curly-brace code=" {" />
+        </code-line>
+        <code-line level-3>
+          <mu-style-param code="margin" />:
+          <mu-number code="1.5" />
+          <mu-style-unit-meas code="em" />
+          <mu-number code=" 0 " />;
+        </code-line>
+        <code-line level-2>
+          <mu-style-curly-brace code="}" />
+        </code-line>
+
+        <code-line>
+          <mu-style-class code="}" />
+        </code-line>
 
         <code-line v-for="i in 8" :key="i" />
       </template>
 
       <template #code>
-
+        
       </template>
     </com-imports>
   </div>
