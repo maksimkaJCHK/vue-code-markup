1aeb566085da76f7d1e81dd51c204952768fded7
maksimkaJCHK <maksimkajchk@rambler.ru>
2025-12-09 21:13:31 +0300

    Не много дописал документацию по слотам

--- src/components/doc/SlotsDoc.vue
+++ src/components/doc/SlotsDoc.vue
@@ -8,12 +8,42 @@
       </p>
 
       <slots-example lang="ru" />
+
+      <p>
+        Я думаю, пример выше отлично показывает как пользоваться слотами, но всё же ниже я их опишу.
+      </p>
+
+      <ul>
+        <li>
+          <strong>header</strong> - 
+        </li>
+        <li>
+          <strong>default</strong> - 
+        </li>
+        <li>
+          <strong>copy</strong> - 
+        </li>
+        <li>
+          <strong>success</strong> - 
+        </li>
+        <li>
+          <strong>error</strong> - 
+        </li>
+      </ul>
     </template>
 
     <template v-if="!props.isRus">
       <h2>Slots</h2>
 
+      <p>
+        For this section, it's worth starting with an example right away, so it will be clearer:
+      </p>
+
       <slots-example />
+
+      <p>
+        I think the example above perfectly shows how to use the slots, but still I will describe them below.
+      </p>
     </template>
   </div>
 </template>
