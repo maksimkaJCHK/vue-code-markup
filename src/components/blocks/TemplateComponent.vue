7e64f07d67084b7afe12e45792280116025726e0
maksimkaJCHK <maksimkajchk@rambler.ru>
2025-12-09 19:31:27 +0300

    Сделал компоненты для примера блока шаблонов

---
+++ src/components/blocks/TemplateComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <code-line level-3>
+    <mu-tag code="<template " />
+    <mu-attr :code="`#${ props.type }`" />
+    <mu-tag code=">" />
+  </code-line>
+
+  <code-line level-4>
+    <slot />
+  </code-line>
+
+  <code-line level-3>
+    <mu-tag code="</template>" />
+  </code-line>
+</template>
+
+<script setup>
+  const props = defineProps(['type']);
+</script>
--- src/components/code-examples/doc/SlotsExample.vue
+++ src/components/code-examples/doc/SlotsExample.vue
@@ -4,14 +4,44 @@
     header="Slots example"
     v-bind="codeParam"
   >
-    <template #default>
+    <template-block>
 
-    </template>
+      <code-line level-2>
+        <mu-tag code="<code-markup>" />
+      </code-line>
+
+      <template-component type="header">
+        <mu-comment :code="`<!-- ${headerText} -->`" />
+      </template-component>
+
+      <template-component type="default">
+        <mu-comment :code="`<!-- ${defaultText} -->`" />
+      </template-component>
+
+      <template-component type="default">
+        <mu-comment :code="`<!-- ${defaultText} -->`" />
+      </template-component>
+
+      <code-line level-2>
+        <mu-tag code="</code-markup>" />
+      </code-line>
+
+    </template-block>
+
+    <code-line />
+    <script-block>
+
+    </script-block>
   </code-markup>
 </template>
 
 <script setup>
+  import { computed } from 'vue';
   import useLang from '../useLang';
+
+  import TemplateBlock from '@/components/blocks/TemplateBlock.vue';
+  import ScriptBlock from '@/components/blocks/ScriptBlock.vue';
+  import TemplateComponent from '@/components/blocks/TemplateComponent.vue';
 
   const props = defineProps({
     lang: {
@@ -26,6 +56,18 @@
   const { codeParam } = useLang(props);
 
   const code = ``;
+
+  const headerText = computed(() => {
+    if (props.lang === 'ru') return 'Сюда вставляется заголвок окна с кодом';
+
+    return 'The header of the window with the code is inserted here';
+  });
+
+  const defaultText = computed(() => {
+    if (props.lang === 'ru') return 'Сюда вставляется код по умолчанию, если никаких других шаблонов не задано';
+
+    return 'The default code is inserted here if no other templates are specified';
+  });
 </script>
 
 <style lang="scss" scoped>
