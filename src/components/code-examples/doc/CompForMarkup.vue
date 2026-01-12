14ef2eb58b64d03d9127d3eb768e8537f65959c0
maksimkaJCHK <maksimkajchk@rambler.ru>
2026-01-12 22:00:09 +0300

    Дописал документацию для компонентов разметки документа

---
+++ src/components/code-examples/doc/CompForMarkup.vue
@@ -0,0 +1,51 @@
+<template>
+  <code-markup
+    :code="code"
+    :is-header="false"
+    v-bind="codeParam"
+  >
+    <template-block>
+      <code-line level-2>
+        <mu-tag code="<div " />
+        <mu-attr code="class=" />
+        <mu-text code='"common-class ' />
+        <mu-text
+          code="old-class"
+          line-through
+        />
+        <mu-text code=" new-class" />
+        <mu-text code='"' />
+        <mu-tag code=">" />
+      </code-line>
+      <code-line level-2>
+        <mu-tag code="</div>" />
+      </code-line>
+    </template-block>
+  </code-markup>
+</template>
+
+<script setup>
+  import useLang from '../useLang';
+  import TemplateBlock from '@/components/blocks/TemplateBlock.vue';
+
+  const props = defineProps({
+    lang: {
+      type: String,
+      default: 'en',
+      validator(value) {
+        return ['ru', 'en'].includes(value);
+      }
+    }
+  });
+
+  const { codeParam } = useLang(props);
+
+  const code = `<template>
+  <div class="common-class old-class new-class">
+  </div>
+</template>`;
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/doc/MarkUpDoc.vue
+++ src/components/doc/MarkUpDoc.vue
@@ -6,8 +6,23 @@
       </h2>
 
       <p>
-        Если сравнивать с HTML, то эти компоненты можно назвать тегами из которых состоит документ. Все эти компоненты имеют один входной параметр, он называется "code", для удобства ими можно обернуть текст кода, но я бы посоветовал всё таки пользоваться входным параметром. Если он задан, то выведится содержимое входного параметра "code".
+        Если сравнивать с HTML, то эти компоненты можно назвать тегами из которых состоит документ. Все эти компоненты имеют следующие входные параметры:
       </p>
+
+      <ul>
+        <li>
+          <strong>code</strong> - входной параметр в который передаётся текст для отображения элементов кода. Для удобства можно обойтись без данного параметры если обернуть текст этими компонентами, но я бы посоветовал вам всё таки пользоваться данным входным параметром. Если он задан, то выведится содержимое входного параметра "code".
+        </li>
+        <li>
+          <strong>lineThrough</strong> - данный входной параметр делает код зачеркнутым. 
+        </li>
+      </ul>
+
+      <p>
+        Ниже я приведу пример, чтобы было понятнее. Во второй строке я сделал "old-class" зачёркнутым.
+      </p>
+
+      <comp-for-markup lang="ru" />
 
       <p>
         Условно все компоненты для разметки документа можно разделить на общие компоненты, компоненты в блоке шаблона, компоненты в блоке скриптов, компоненты в блоке стилей.
@@ -20,8 +35,23 @@
       </h2>
 
       <p>
-        When compared with HTML, these components can be called the tags that make up the document. All these components have one input parameter, it is called "code", for convenience, they can wrap the text of the code, but I would advise you to use the input parameter anyway. If it is set, the contents of the input parameter "code" will be output.
+        When compared with HTML, these components can be called the tags that make up the document. All these components have the following input parameters:
       </p>
+
+      <ul>
+        <li>
+          <strong>code</strong> is the input parameter to which the text is passed to display the code elements. For convenience, you can do without these parameters if you wrap the text with these components, but I would advise you to still use this input parameter. If it is set, the contents of the input parameter "code" will be output.
+        </li>
+        <li>
+          <strong>lineThrough</strong> - this input parameter makes the code strikethrough. 
+        </li>
+      </ul>
+
+      <p>
+        I will give an example below to make it clearer. I've strikethroughed "old-class" in the second line.
+      </p>
+
+      <comp-for-markup />
 
       <p>
         Conventionally, all components for marking up a document can be divided into common components, components in the template block, components in the script block, and components in the styles block.
@@ -31,6 +61,8 @@
 </template>
 
 <script setup>
+  import CompForMarkup from '@/components/code-examples/doc/CompForMarkup.vue';
+
   const props = defineProps({
     isRus: {
       type: Boolean,
